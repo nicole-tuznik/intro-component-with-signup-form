72e04ce01a85d47abfd47cb403ecea56f90693a6
nicole-tuznik <69260034+nicole-tuznik@users.noreply.github.com>
2021-01-19 16:05:42 -0500

    JavaScript and error styling

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-const form = document.querySelector('.form');
 let inputs = document.querySelectorAll('.form--field');
-let error = document.querySelectorAll('.form--error');
+let formBtn = document.querySelector('.form__button');
 
+
+formBtn.addEventListener('click', function(e){
+  for(let i = 0; i < inputs.length; i++) {
+    let currentInput = inputs[i];
+    let inputParent = currentInput.parentNode;
+
+    if(!inputs[i].validity.valid) {
+
+      inputParent.classList.add('error');
+      e.preventDefault();
+    } else {
+      inputParent.classList.remove('error');
+    }
+  };
+});
