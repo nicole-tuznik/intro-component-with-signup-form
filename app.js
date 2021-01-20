12360a8dc63f4fc1c7840d8de57c035a715acc5a
nicole-tuznik <69260034+nicole-tuznik@users.noreply.github.com>
2021-01-20 14:17:40 -0500

    Final styling

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-let inputs = document.querySelectorAll('.form--field');
+let formFields = document.querySelectorAll('.form--field');
 let formBtn = document.querySelector('.form__button');
 
 
 formBtn.addEventListener('click', function(e){
-  for(let i = 0; i < inputs.length; i++) {
-    let currentInput = inputs[i];
+  for(let i = 0; i < formFields.length; i++) {
+    let currentInput = formFields[i];
     let inputParent = currentInput.parentNode;
 
-    if(!inputs[i].validity.valid) {
+    // If input is invald and is NOT the email field, add error class and remove placeholder
+    if((!currentInput.validity.valid) && (currentInput.getAttribute('id') !== 'email')) {
 
       inputParent.classList.add('error');
+      currentInput.removeAttribute('placeholder');
       e.preventDefault();
+    
+      // If input is invalid and it IS the email field, add error class and change placeholder value
+    } else if ((!currentInput.validity.valid) && (currentInput.getAttribute('id') === 'email')) {
+      inputParent.classList.add('error');
+      currentInput.placeholder = `email@example.com`;
+      e.preventDefault();
+    
     } else {
       inputParent.classList.remove('error');
     }
